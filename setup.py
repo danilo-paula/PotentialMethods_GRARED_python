24afa6c7b658a9538fe1fdc0b52432f594da6dee
geolit <geolit.iag.usp@gmail.com>
2017-12-13 14:37:41 -0200

    Troca steup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import cx_Freeze
+import os
 import numpy as np
 import pandas as pd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 from tkinter import *
-import sys
-import os
+from datetime import datetime
+from math import sqrt, atan, asin, acos, sin, cos, radians
+
 
 
 base = None
@@ -15,14 +17,13 @@ if sys.platform == 'win32':
 
 executables = [cx_Freeze.Executable("GRARED.py", base=base, icon="clienticon.ico")]
 
-
-os.environ['TCL_LIBRARY'] = r'C:\Program Files\Python35-32\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'C:\Program Files\Python35-32\tcl\tk8.6'
+os.environ['TCL_LIBRARY'] = r'C:\Python35-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Python35-32\tcl\tk8.6'
 
 cx_Freeze.setup(
     name = "GRARED",
-    options = {"build_exe": {"packages":["tkinter","pandas","numpy"], "include_files":["clienticon.ico"]}},
-    version = "Alpha 0.2",
+    options = {"build_exe": {"packages":["numpy","pandas","tkinter","datetime","math"], "include_files":["clienticon.ico"]}},
+    version = "Alpha 0.5",
     description = "Gravimetric Data Reduction",
     executables = executables
     )
