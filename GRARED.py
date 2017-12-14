161149a17373363692921c7d64e760c65788c5fd
Danilo de Paula <danilo_p@usp.br>
2017-12-14 15:50:51 -0200

    Update da Licença utiliza por John Leeman na Força de Maré

--- GRARED.py
+++ GRARED.py
@@ -28,7 +28,32 @@ from math import sqrt, atan, asin, acos, sin, cos, radians
 # Code of calculation of Julian centuries and Earth Tidal Correction
 # by John Leeman  (contact: <john@leemangeophysical.com>  )
 # available at: <https://github.com/jrleeman/LongmanTide>
-           
+
+"""
+This part of the code provided by John Leeman uses the MIT License, which is written just below:
+
++MIT License
+ +
+ +Copyright (c) 2017 John Leeman
+ +
+ +Permission is hereby granted, free of charge, to any person obtaining a copy
+ +of this software and associated documentation files (the "Software"), to deal
+ +in the Software without restriction, including without limitation the rights
+ +to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ +copies of the Software, and to permit persons to whom the Software is
+ +furnished to do so, subject to the following conditions:
+ +
+ +The above copyright notice and this permission notice shall be included in all
+ +copies or substantial portions of the Software.
+ +
+ +THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ +IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ +FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ +AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ +LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ +OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ +SOFTWARE.
+"""           
 
 class TideModel():
     def calculate_julian_century(self, timestamp):
